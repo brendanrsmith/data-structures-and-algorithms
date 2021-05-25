bb3c5a3c87227a0d3d176ccaa834684c4142dca7
Brendan Smith <brendan5320@gmail.com>
2021-05-25 17:27:11 -0500

    working, sort of?

--- javascript/code-challenges/mergeSort/index.js
+++ javascript/code-challenges/mergeSort/index.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const mergeSort = require('./merge-sort');
+
+let arr = [8,4,23,42,16,15];
+console.log(arr);
+mergeSort(arr);
+console.log(arr);
+
+
+let arr2 = [ 4, 8, 15, 16, 23, 42 ];
+console.log(arr2);
+mergeSort(arr2);
+console.log(arr2);
--- javascript/code-challenges/mergeSort/merge-sort.js
+++ javascript/code-challenges/mergeSort/merge-sort.js
@@ -1,2 +1,53 @@
 'use strict';
 
+function mergeSort(arr) {
+  const n = arr.length;
+
+  if (n > 1) {
+    const mid = Math.floor(n / 2);
+    const left = arr.splice(0, mid);
+    const right = arr.splice(0);
+    // console.log('n', n);
+    // console.log('mid', mid);
+    // console.log('left', left);
+    // console.log('right', right);
+    mergeSort(left);
+    mergeSort(right);
+    merge(left, right, arr);
+  }
+
+  return arr;
+}
+
+function merge(left, right, arr) {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  // console.log('left', left);
+  // console.log('right', right);
+  // console.log('arr', arr);
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      arr[k] = left[i];
+      i += 1;
+    } else {
+      arr[k] = right[j];
+      j += 1;
+    }
+    k += 1;
+  }
+
+  if (i === left.length) {
+    while (j < right.length) {
+      arr[k] = right[j];
+      j += 1;
+    }
+  } else {
+    while (i < left.length) {
+      arr[k] = left[i];
+      i += 1;
+    }
+  }
+}
+
+module.exports = mergeSort;
